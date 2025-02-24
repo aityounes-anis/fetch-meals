7bcc2f768b95c845394fea69a431740812b8fb64
aityounes-anis <ma.mohamed.aityounes@gmail.com>
2025-02-24 16:21:28 +0000

    handle filter saerch

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,10 +1,29 @@
+import { useContext, useRef } from "react";
 import { BiSearch } from "react-icons/bi";
+import { MealsContext, SetDisplayedMealsContext } from "../App";
 
 const SearchBar = () => {
+  const meals = useContext(MealsContext);
+  const setDisplayedMeals = useContext(SetDisplayedMealsContext);
+
+  const inputRef = useRef(null);
+
+  const filterMeals = () => {
+    const newMeals = meals.filter((meal) =>
+      meal.strMeal.toLowerCase().startsWith(inputRef.current.value)
+    );
+    setDisplayedMeals(newMeals);
+  };
+
   return (
     <section>
       <div className="search-bar">
-        <input type="text" placeholder="Search for a meal..." />
+        <input
+          type="text"
+          placeholder="Search for a meal..."
+          ref={inputRef}
+          onChange={filterMeals}
+        />
         <button type="button">
           <BiSearch />
         </button>
